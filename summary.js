5bdb5d9246eff0f6b7abc80f6224bca510db7dfe
jerinjamandev <jerinjaman.dev@gmail.com>
2025-09-19 20:37:47 +0600

    add

---
+++ summary.js
@@ -0,0 +1,47 @@
+// var a=10;
+// console.log(a);
+
+// Google Chrome utilizes the V8 JavaScript engine.
+
+// javascript data type :1.string 2. boolean 3.number 4.array 5.object 6.bigint 7.undefinf 8.null
+
+// primitive type : 1.string 2.boolean 3.number 4.null 5.bigint 6.undefind
+
+//  non-primitive: 1.array 2.object 
+
+
+// let name='jerin';
+
+// let name2=name;
+
+// name ='jaman'
+// console.log(name)
+// console.log(name2)
+
+
+// var number=10;
+// var number2=number;
+
+// number=22;
+
+
+// console.log(number)
+// console.log(number2)
+
+
+// let name=['jerin','ratul','riya','piya']
+
+// let name2=name;
+
+// // spread oparetor
+// let name3=[...name]
+// name.pop()
+// console.log(name);
+// console.log(name2);
+// console.log(name3)
+
+
+
+// window browser er property node js er na ...javascript egula browser theke chalay 
+console.log(window)
+
