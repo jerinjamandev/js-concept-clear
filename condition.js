1730574d4284ac89e0444b850c6ddc697c45443d
jerinjamandev <jerinjaman.dev@gmail.com>
2025-09-19 21:02:31 +0600

    add2

---
+++ condition.js
@@ -0,0 +1,42 @@
+// basic conditions
+
+// if-else...2.else-if..3.switch condition...4.ternarry condition
+
+// const age =-23;
+
+// if(age>=18){
+//     console.log('adult');
+
+// }
+
+// else{console.log('not adult');
+// }
+
+
+// if-else/else-if
+
+// const mark = 28;
+
+// if (mark >= 90) {
+//     console.log('a+');
+
+// }
+// else if (mark >= 70) {
+//     console.log('A');
+
+// }
+// else {
+//     console.log('faild');
+// }
+
+
+// ternary oparetor
+
+
+const age=17;
+
+const result=(age>=18)?'adult':'not-adult';
+
+console.log(result);
+
+
